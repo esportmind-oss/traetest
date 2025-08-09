3d5b5906c644906e7985417a35ed43f42a45ef9b
esportmind-oss <esportmind@gmail.com>
2025-08-10 02:30:26 +0700

    Update index.js
    oke

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Import semua route dari folder backend/src/routes
+const userRoutes = require('./backend/src/routes/userRoutes');
+const customerRoutes = require('./backend/src/routes/customerRoutes');
+const meterRoutes = require('./backend/src/routes/meterRoutes');
+const reportRoutes = require('./backend/src/routes/reportRoutes');
+
+// Gunakan semua route
+app.use('/users', userRoutes);
+app.use('/customers', customerRoutes);
+app.use('/meters', meterRoutes);
+app.use('/reports', reportRoutes);
 
 app.get('/', (req, res) => {
   res.send('Aplikasi berhasil dijalankan!');
